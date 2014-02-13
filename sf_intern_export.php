bde46b452edf6509297a75059e0ad7a3731f2633
Ali Nowicki (work PC) <ali@techmission.org>
2014-02-13 11:17:37 -0500

    Debugging

--- sf_intern_export.php
+++ sf_intern_export.php
@@ -32,6 +32,7 @@ function run_cron() {
   }
   
   $cv_objects = query_sf_objects($sf);
+  print_r($cv_objects);
   
   if((isset($cv_objects[OBJECT_TYPE_LEAD]) && count($cv_objects[OBJECT_TYPE_LEAD]) > 0) || (isset($cv_objects[OBJECT_TYPE_CONTACT]) && count($cv_objects[OBJECT_TYPE_CONTACT]) > 0)) {
     $results = write_cv_objects($cv_objects);
